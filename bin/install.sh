fe948d1601fb41f7d9c314a25df18fc8ae264a52
Mateusz <mateusz.pytel@grupakmk.pl>
2020-11-30 14:48:26 +0100

    Installation script fixed

--- bin/install.sh
+++ bin/install.sh
@@ -7,7 +7,6 @@ PLUGIN_NAME=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
-echo "Installing LSF plugin" $PLUGIN_NAME
+echo "Installing LSF plugin" $PLUGIN_NAME "..."
 mkdir -p $cwd/out
-cp src/index.js out/index.js
-
+cp src/index.js ../../out/index.js
