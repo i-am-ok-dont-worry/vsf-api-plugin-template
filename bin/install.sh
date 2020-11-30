3feb9a732edd95b82bc182dc937e4b26ac0c0186
i-am-ok-dont-worry <mateuspytel@gmail.com>
2020-11-30 20:14:48 +0100

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
 cwd=$(pwd)
-PLUGIN_NAME=$(cat package.json \
-  | grep pluginname \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
 
-COMPANY=$(cat package.json | grep name | sed 's/.*"name": "\(.*\)".*/\1/')
+PLUGIN_NAME=$(cat package.json | grep pluginname | sed 's/.*"pluginname": "\(.*\)".*/\1/')
+COMPANY=$(cat package.json | grep companyname | sed 's/.*"companyname": "\(.*\)".*/\1/')
 echo "Installing LSF plugin" $PLUGIN_NAME from $COMPANY "..."
 
-mkdir -p "$cwd/../../vendor/@$COMPANY"
-cp -R src/ ../../vendor/@$COMPANY
-cp README.md ../../vendor/@$COMPANY/README.md
+INSTALLATION_PATH="../"
+if [[ -f "../package.json" ]]
+then
+  INSTALLATION_PATH="../";
+elif [[ -f "../../package.json" ]]
+then
+  INSTALLATION_PATH="../.."
+elif [[ -f "../../../package.json" ]]
+then
+  INSTALLATION_PATH="../../.."
+elif [[ -f "../../../../package.json" ]]
+then
+  INSTALLATION_PATH="../../../.."
+else
+  echo "Cannot install script. Invalid API structure"
+  exit 1
+fi;
+
+mkdir -p "$cwd/$INSTALLATION_PATH/vendor/@$COMPANY/$PLUGIN_NAME"
+cp -R src/* "./$INSTALLATION_PATH/vendor/@$COMPANY/$PLUGIN_NAME"
+cp README.md "./$INSTALLATION_PATH/vendor/@$COMPANY/$PLUGIN_NAME/README.md"
