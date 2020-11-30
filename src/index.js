d2dd05a05527327fd3c92e4c2c20f813ed7c6379
i-am-ok-dont-worry <mateuspytel@gmail.com>
2020-11-30 23:40:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = ({ config, db, router, cache, apiStatus, apiError, getRestApiClient }) => {
     console.warn('----> Plugin initialized');
+    
+    return {
+        domainName: '{{domainName}}',
+        pluginName: '{{pluginName}}',
+        route: '{{restRoute}}',
+        router
+    };
 };
