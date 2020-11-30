6103dc03d1a9fb311a6c775ff1b36bacc80a8ee1
Mateusz <mateusz.pytel@grupakmk.pl>
2020-11-30 14:38:49 +0100

    Installation file added

---
+++ bin/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cwd=$(pwd)
+PLUGIN_NAME=$(cat package.json \
+  | grep name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+echo "Installing LSF plugin" $PLUGIN_NAME
+mkdir -p $cwd/out
+cp src/index.js out/index.js
+
